ed4deade73de060463da333a90ac259eec6400cd
ahnasr <44976512+ahnasr@users.noreply.github.com>
2018-11-19 13:14:19 -0800

    Add files via upload

---
+++ chat/fullChat.js
@@ -0,0 +1,36 @@
+const client = new ChimeWebSDK();
+var currentUser;
+
+window.addEventListener('load', function() {
+	setTimeout(init, 500);
+});
+
+function init() {
+	client.auth.checkIsAuthenticated().then(setLoginStatus);
+	client.auth.onAuthStatus(setLoginStatus);
+}
+
+function login() {
+	client.auth.authenticate();
+}
+
+function logout() {
+	click.auth.signout();
+}
+
+function setLoginStatus(isAuthenticated) {
+	document.getElementById("login-link").style.display = (isAuthenticated)?"none" : "block";
+	document.getElementById("logout-link").style.display = (isAuthenticated)?"block" : "none";
+	document.getElementById("loading-div").style.display = "none";
+	document.getElementById("main-tab").style.display = "block";
+	
+	if(isAuthenticated)
+		client.auth.getCurrentUserProfile().then(setCurrentUser); 
+	else
+		document.getElementById("currentUser").innerHTML = "";
+}
+
+function setCurrentUser(profile) {
+	currentUser = profile;
+	currentUser.innerHTML = currentUser.name;
+}
