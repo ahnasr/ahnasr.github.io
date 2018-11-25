ef2e365a1dba247ffc0da419b01cf6db77fc5a1c
Nasr <nasran@amazon.com>
2018-11-25 08:55:54 -0800

    c6

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-window.addEventListener('load', function() {document.body.innerHTML+= "<br />ADDED BY JS, great no JS cache5";});
+window.addEventListener('load', function() {document.body.innerHTML+= "<br />ADDED BY JS, great no JS cache6";});
